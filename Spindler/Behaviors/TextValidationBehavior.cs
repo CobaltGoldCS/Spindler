b5bc9d8882f61ece78c6381799c417dfe1022ec7
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-07-22 22:31:59 -0600

    Add a full-on validation behavior for entry

---
+++ Spindler/Behaviors/TextValidationBehavior.cs
@@ -0,0 +1,28 @@
+﻿namespace Spindler.Behaviors
+{
+    public class TextValidationBehavior : Behavior<Entry>
+    {
+        public Func<string, bool> validationFunction;
+        public TextValidationBehavior(Func<string, bool> validationFunction)
+        {
+            this.validationFunction = validationFunction;
+        }
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            bool isValid = validationFunction.Invoke(args.NewTextValue);
+            ((Entry)sender).TextColor = isValid ? Colors.Black : Colors.Red;
+        }
+    }
+}
