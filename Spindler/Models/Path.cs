7c7e96a770c277b0d00b6bf8519ff517eb8f72c9
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-07-31 20:36:32 -0600

    Add docstrings for Path

--- Spindler/Models/Path.cs
+++ Spindler/Models/Path.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Spindler.Models
 {
+    /// <summary>
+    /// Dataclass representing a selector path, such as an xpath or csspath
+    /// </summary>
     public class Path
     {
         public enum Type
