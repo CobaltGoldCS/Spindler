a6fc5120cfd1ada274a7f8cd52f17ad835e74229
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-07-31 20:36:46 -0600

    Remove Unused imports

--- Spindler/ViewModels/ReaderViewModel.cs
+++ Spindler/ViewModels/ReaderViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using Spindler.Services;
 using Spindler.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
