81786b5d1354e5a5fc992f49017e86deb59848ec
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-08-01 19:19:04 -0600

    Create a Generalized Configuration Model

---
+++ Spindler/Models/GeneralizedConfig.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spindler.Models
+{
+    public class GeneralizedConfig : Config
+    {
+        /// <summary>
+        /// The path to check in order to see if this configuration is valid
+        /// </summary>
+        public string MatchPath { get; set; }
+    }
+}
