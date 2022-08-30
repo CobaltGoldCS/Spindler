ed12c7ec247fbd4d167edd45741ba0b8586f994c
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-08-30 10:31:16 -0600

    Add GetOrDefault Extension method to dictionary
    This is really useful for getting a value that is not guaranteed to be
    present in the dictionary

---
+++ Spindler/Utils/Extensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Spindler.Utils
+{
+    public static class Extensions
+    {
+        public static Value GetOrDefault<Key, Value>(this Dictionary<Key, Value> dict, Key key, Value @default)
+        {
+            Value value;
+            return dict.TryGetValue(key, out value) ? value : @default;
+        }
+    }
+}
