933e357e434c2cdf1b0f747d3f7041f8c4782e64
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-09-29 09:47:08 -0600

    remove unnecessary usings

--- Spindler/Models/Path.cs
+++ Spindler/Models/Path.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Spindler.Models;
+﻿namespace Spindler.Models;
 
 /// <summary>
 /// Dataclass representing a selector path, such as an xpath or csspath
