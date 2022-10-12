8e2f444b43f27f01721592f34413130194ff3aea
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-10-12 08:45:29 -0600

    Fix nullability warnings, and fix null cookies

--- Spindler/Platforms/Android/MainActivity.cs
+++ Spindler/Platforms/Android/MainActivity.cs
@@ -8,8 +8,10 @@ namespace Spindler;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+#nullable disable
     protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
     }
+#nullable enable
 }
