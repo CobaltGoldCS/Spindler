65395900fb08ead8215ee1aa22a6ae2abe656c45
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-11-01 20:37:57 -0600

    Moved config functionality to view model

--- Spindler/ViewModels/ConfigViewModel.cs
+++ Spindler/ViewModels/ConfigViewModel.cs
@@ -1,18 +1,51 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Spindler.Models;
+using Spindler.Services;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace Spindler.ViewModels;
 public partial class ConfigViewModel : ObservableObject
 {
+    [ObservableProperty]
+    public Config? selectedItem;
 
-    public ICommand AddCommand { get; private set; }
-
-    public ConfigViewModel()
+    private List<Config>? _configItems;
+    // I don't know what's happening here, but for some reason I have to do this in order to refresh the UI
+    public List<Config>? ConfigItems
     {
-
-        AddCommand = new Command(async () =>
+        get => _configItems;
+        set
         {
-            await Shell.Current.GoToAsync($"/{nameof(ConfigDetailPage)}?id=-1");
-        });
+            if (_configItems != value)
+            {
+                SetProperty(ref _configItems, value, nameof(ConfigItems));
+            }
+        }    
+    }
+
+    [ObservableProperty]
+    public bool isRefreshing = false;
+    
+    [RelayCommand]
+    public async Task ReloadItems()
+    {
+        IsRefreshing = true;
+        ConfigItems = await App.Database.GetAllItemsAsync<Config>();
+        IsRefreshing = false;
+    }
+
+    [RelayCommand]
+    public async Task ItemClicked()
+    {
+        await Shell.Current.GoToAsync($"/{nameof(ConfigDetailPage)}?id={selectedItem!.Id}");
+    }
+
+    [RelayCommand]
+    public async Task Add()
+    {
+        await Shell.Current.GoToAsync($"/{nameof(ConfigDetailPage)}?id=-1");
     }
 }
--- Spindler/Views/Configuration Pages/ConfigPage.xaml.cs
+++ Spindler/Views/Configuration Pages/ConfigPage.xaml.cs
@@ -7,22 +7,7 @@ public partial class ConfigPage : ContentPage
     public ConfigPage()
     {
         InitializeComponent();
-        BindingContext = new ConfigViewModel();
+        var viewModel = new ConfigViewModel();
+        BindingContext = viewModel;
     }
-
-    protected async override void OnAppearing()
-    {
-        base.OnAppearing();
-        list.Unfocus();
-        list.ItemsSource = await App.Database.GetAllItemsAsync<Config>();
-    }
-
-    #region Click Handlers
-
-    private async void list_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        Config config = (Config)e.CurrentSelection.FirstOrDefault()!;
-        await Shell.Current.GoToAsync($"/{nameof(ConfigDetailPage)}?id={config.Id}");
-    }
-    #endregion
 }
