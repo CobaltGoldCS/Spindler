50eec34fbef1ce57528378855db1b42b33a882c1
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-11-01 21:05:27 -0600

    Split generalized config into viewmodel; added empty fallback

--- Spindler/Views/Configuration Pages/GeneralizedConfigPage.xaml.cs
+++ Spindler/Views/Configuration Pages/GeneralizedConfigPage.xaml.cs
@@ -8,27 +8,6 @@ public partial class GeneralizedConfigPage : ContentPage
 	public GeneralizedConfigPage()
 	{
 		InitializeComponent();
-    }
-
-    protected async override void OnAppearing()
-    {
-        base.OnAppearing();
-        list.ItemsSource = await App.Database.GetAllItemsAsync<GeneralizedConfig>();
-        list.Unfocus();
-    }
-
-    #region Click Handlers
-
-    private async void list_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        GeneralizedConfig config = (GeneralizedConfig)e.CurrentSelection.FirstOrDefault()!;
-        list.Unfocus();
-        await Shell.Current.GoToAsync($"/{nameof(GeneralizedConfigDetailPage)}?id={config.Id}");
-    }
-    #endregion
-
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync($"/{nameof(GeneralizedConfigDetailPage)}?id=-1");
+        BindingContext = new GeneralizedConfigViewModel();
     }
 }
