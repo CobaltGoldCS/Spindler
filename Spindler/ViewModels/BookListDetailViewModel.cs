39c54cd5284f11357f2e17b61757a47a3ed78fc7
Dylan Green <cobaltgoldcsprinks@gmail.com>
2022-11-09 18:24:15 -0700

    Add color pickers and move things to viewmodel

---
+++ Spindler/ViewModels/BookListDetailViewModel.cs
@@ -0,0 +1,109 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Spindler.Models;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Spindler.ViewModels
+{
+    public partial class BookListDetailViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public ChooseColor chosenColor1;
+        [ObservableProperty]
+        public ChooseColor chosenColor2;
+        [ObservableProperty]
+        public static IList<ChooseColor> colorList = new List<ChooseColor>()
+        {
+            new ChooseColor { color = Colors.Black },
+            new ChooseColor { color = Colors.White },
+            new ChooseColor { color = Colors.Wheat },
+            new ChooseColor { color = Colors.Thistle },
+            new ChooseColor { color = Colors.AliceBlue },
+            new ChooseColor { color = Colors.Aqua },
+            new ChooseColor { color = Colors.Aquamarine },
+            new ChooseColor { color = Colors.Blue },
+            new ChooseColor { color = Colors.Green },
+            new ChooseColor { color = Colors.ForestGreen },
+            new ChooseColor { color = Colors.LawnGreen },
+            new ChooseColor { color = Colors.LightSeaGreen },
+            new ChooseColor { color = Colors.Orange },
+            new ChooseColor { color = Colors.Yellow },
+            new ChooseColor { color = Colors.Turquoise },
+            new ChooseColor { color = Colors.Tomato },
+            new ChooseColor { color = Colors.Red },
+            new ChooseColor { color = Colors.Purple },
+            new ChooseColor { color = Colors.Pink },
+         };
+        // COLORS ARE NOT UPDATING AGAIN
+        BookList booklist;
+        public BookList Booklist
+        {
+            get => booklist;
+            set
+            {
+                SetProperty(ref booklist, value);
+            }
+        }
+
+        public BookListDetailViewModel(BookList Booklist)
+        {
+            this.Booklist = Booklist;
+            booklist = Booklist;
+            
+        }
+
+        private async Task Close()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        #region Click Handlers
+
+        [RelayCommand]
+        private async void Ok()
+        {
+            if (!string.IsNullOrWhiteSpace(Booklist.Name))
+            {
+                Booklist.LastAccessed = DateTime.UtcNow;
+                if (Booklist.ImageUrl.Length == 0)
+                {
+                    Booklist.ImageUrl = BookList.GetRandomPlaceholderImageUrl();
+                }
+                await App.Database.SaveItemAsync(Booklist);
+            }
+            await Close();
+        }
+
+        [RelayCommand]
+        private async void Delete()
+        {
+            if (Booklist.Id > 0)
+            {
+                await App.Database.DeleteBookListAsync(Booklist);
+            }
+            await Close();
+        }
+
+        [RelayCommand]
+        private async void Cancel()
+        {
+            await Close();
+        }
+
+        [RelayCommand]
+        public void Color1Picked()
+        {
+            Booklist.Color1 = ChosenColor1.color;
+        }
+
+        [RelayCommand]
+        public void Color2Picked()
+        {
+            Booklist.Color2 = ChosenColor2.color;
+        }
+
+        #endregion
+    }
+}
--- Spindler/Views/Book List Page/BookListDetailPage.xaml.cs
+++ Spindler/Views/Book List Page/BookListDetailPage.xaml.cs
@@ -1,5 +1,7 @@
 using Spindler.Behaviors;
 using Spindler.Models;
+using Spindler.ViewModels;
+using System.Linq;
 
 namespace Spindler;
 
@@ -7,7 +9,7 @@ namespace Spindler;
 public partial class BookListDetailPage : ContentPage
 {
     #region QueryProperty handler
-    private BookList _booklist = new BookList();
+    private BookList _booklist = new();
     public BookList Booklist
     {
         set
@@ -25,22 +27,28 @@ public partial class BookListDetailPage : ContentPage
         {
             okButton.Text = "Add";
             Title = "Add a new Book List";
-            return;
         }
-        okButton.Text = $"Modify {booklist.Name}";
-        Title = $"Modify {booklist.Name}";
-        BindingContext = booklist;
+        else
+        {
+            okButton.Text = $"Modify {booklist.Name}";
+            Title = $"Modify {booklist.Name}";
+        }
+        if (!BookListDetailViewModel.colorList.Contains(new ChooseColor { color = booklist.Color1 }))
+            Booklist.Color1 = Colors.Black;
+        if (!BookListDetailViewModel.colorList.Contains(new ChooseColor { color = booklist.Color2 }))
+            Booklist.Color2 = Colors.Black;
+        BindingContext = new BookListDetailViewModel(booklist);
     }
     #endregion
 
     public BookListDetailPage()
     {
         InitializeComponent();
-        imageUrlEntry.Behaviors.Add(new TextValidationBehavior((string text) =>
+        /*UrlEntry.Behaviors.Add(new TextValidationBehavior((string text) =>
         {
             bool validUrl = Uri.TryCreate(text, UriKind.Absolute, out Uri? uriresult) && (uriresult.Scheme == Uri.UriSchemeHttp || uriresult.Scheme == Uri.UriSchemeHttps);
             return validUrl || text.Length == 0;
-        }));
+        }));*/
     }
 
     private async Task Close()
@@ -49,38 +57,4 @@ public partial class BookListDetailPage : ContentPage
         Unfocus();
         await Shell.Current.GoToAsync("..");
     }
-
-    #region Click Handlers
-
-    private async void okButton_Clicked(object sender, EventArgs e)
-    {
-        if (!string.IsNullOrWhiteSpace(nameEntry.Text))
-        {
-            Booklist.ImageUrl = imageUrlEntry.Text;
-            Booklist.Name = nameEntry.Text;
-            Booklist.LastAccessed = DateTime.UtcNow;
-            if (Booklist.ImageUrl.Length == 0)
-            {
-                Booklist.ImageUrl = BookList.GetRandomPlaceholderImageUrl();
-            }
-            await App.Database.SaveItemAsync(Booklist);
-        }
-        await Close();
-    }
-
-    private async void DeleteButton_clicked(object sender, EventArgs e)
-    {
-        if (Booklist.Id > 0)
-        {
-            await App.Database.DeleteBookListAsync(await App.Database.GetItemByIdAsync<BookList>(Booklist.Id));
-        }
-        await Close();
-    }
-
-    private async void Cancel_Clicked(object sender, EventArgs e)
-    {
-        await Close();
-    }
-
-    #endregion
 }
