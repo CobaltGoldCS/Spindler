e7b4d7b935b2c030998b39bafdc11e2d912c1009
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-02-04 14:57:49 -0700

    Move Configuration items into ConfigItem Control

---
+++ Spindler/CustomControls/Items/ConfigItem.xaml.cs
@@ -0,0 +1,20 @@
+using Spindler.Models;
+
+namespace Spindler.CustomControls.Items;
+
+public partial class ConfigItem : Border
+{
+
+    public static readonly BindableProperty ConfigParameterProperty =
+                BindableProperty.Create(nameof(ConfigParameter), typeof(Config), typeof(ConfigItem));
+
+    public Config ConfigParameter
+    {
+        get => (Config)GetValue(ConfigParameterProperty);
+        set => SetValue(ConfigParameterProperty, value);
+    }
+    public ConfigItem()
+	{
+		InitializeComponent();
+	}
+}
