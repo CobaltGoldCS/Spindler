9190fccfb7c5635181b566820beadf076f998308
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-03-01 13:34:48 -0700

    Inherit BaseConfigDetailPage from GeneralizedConfigDetailPage
    This simplifies the implementation of GeneralizedConfigDetailPage
    
    (And it also allows Generalized Configs to be imported and exported as
    JSON)

--- Spindler/Views/Configuration Pages/GeneralizedConfigDetailPage.xaml.cs
+++ Spindler/Views/Configuration Pages/GeneralizedConfigDetailPage.xaml.cs
@@ -2,13 +2,13 @@ using Spindler.Behaviors;
 using Spindler.Models;
 using Spindler.Services;
 using Spindler.Utilities;
+using Spindler.Views.Configuration_Pages;
 
 namespace Spindler;
 
 [QueryProperty(nameof(config), "config")]
-public partial class GeneralizedConfigDetailPage : ContentPage
+public partial class GeneralizedConfigDetailPage : BaseConfigDetailPage<GeneralizedConfig>
 {
-    private GeneralizedConfig Configuration = new() { Id = -1 };
 
     #region QueryProperty handler
     public GeneralizedConfig config
@@ -21,17 +21,21 @@ public partial class GeneralizedConfigDetailPage : ContentPage
         }
     }
 
-    private void InitializePage(GeneralizedConfig config)
+    protected override void InitializePage(GeneralizedConfig config)
     {
-        if (config.Id < 0)
+        base.InitializePage(config);
+        switch (state)
         {
-            Title = "Add a new Config";
-            Buttons.OkText = "Add";
-        }
-        else
-        {
-            Title = $"Modify {config.DomainName}";
-            Buttons.OkText = "Modify";
+            case State.NewConfig:
+                Buttons.OkText = "Add";
+                importButton.IsEnabled = true;
+                Title = "Add a new Config";
+                break;
+            case State.ModifyConfig:
+                Buttons.OkText = "Modify";
+                exportButton.IsEnabled = true;
+                Title = $"Modify {config.DomainName}";
+                break;
         }
         BindingContext = config;
         switchWebView.On = config.ExtraConfigs.GetOrDefault("webview", false);
@@ -60,15 +64,8 @@ public partial class GeneralizedConfigDetailPage : ContentPage
 
 
     #region Click Handlers
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (config.Id < 0)
-            return;
-        await App.Database.DeleteItemAsync(config);
-        await Shell.Current.GoToAsync("..");
-    }
 
-    private async void okButton_Clicked(object sender, EventArgs e)
+    protected override async void okButton_Clicked(object sender, EventArgs e)
     {
         if (
             !ConfigService.IsValidSelector(matchEntry.Text) ||
@@ -88,14 +85,8 @@ public partial class GeneralizedConfigDetailPage : ContentPage
                             .Replace(@"\t", "     ")},
             { "headless", headlessSwitch.On },
         };
-        if (!ConfigService.IsValidSelector(config.ImageUrlPath))
-            config.ImageUrlPath = "";
-
-        await App.Database.SaveItemAsync(config);
-        await Shell.Current.GoToAsync("..");
+        base.okButton_Clicked(sender, e);
     }
-
-    private async void Cancel_Clicked(object sender, EventArgs e) => await Shell.Current.GoToAsync("..");
 
     #endregion
 }
