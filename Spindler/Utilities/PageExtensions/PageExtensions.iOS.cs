6bb589f7cf70683aeb4680a7f5cd6732a0eaa64e
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-06-26 13:57:01 -0600

    Add the ability to show bottom sheet dialogs

---
+++ Spindler/Utilities/PageExtensions/PageExtensions.iOS.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Maui.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace Spindler.Utilities.PageExtensions;
+
+// https://vladislavantonyuk.github.io/articles/Creating-a-bottom-sheet-using-.NET-MAUI/
+public static partial class PageExtensions
+{
+    public static void ShowBottomSheet(this Page page, IView bottomSheetContent, bool dimDismiss)
+    {
+        var mauiContext = page.Handler?.MauiContext ?? throw new Exception("MauiContext is null");
+        var viewController = page.ToUIViewController(mauiContext);
+        var viewControllerToPresent = bottomSheetContent.ToUIViewController(mauiContext);
+
+        var sheet = viewControllerToPresent.SheetPresentationController;
+        if (sheet is not null)
+        {
+            sheet.Detents = new[]
+            {
+                UISheetPresentationControllerDetent.CreateMediumDetent(),
+                UISheetPresentationControllerDetent.CreateLargeDetent(),
+            };
+            sheet.LargestUndimmedDetentIdentifier = dimDismiss ? UISheetPresentationControllerDetentIdentifier.Unknown : UISheetPresentationControllerDetentIdentifier.Medium;
+            sheet.PrefersScrollingExpandsWhenScrolledToEdge = false;
+            sheet.PrefersEdgeAttachedInCompactHeight = true;
+            sheet.WidthFollowsPreferredContentSizeWhenEdgeAttached = true;
+        }
+
+        viewController.PresentViewController(viewControllerToPresent, animated: true, null);
+    }
+}
