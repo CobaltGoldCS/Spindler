4b803da1a7cc6d5f38363a761bce795d49766c58
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-10-13 12:09:50 -0600

    Create some table control replacements
    This is an initial version for Spindler

---
+++ Spindler/Views/TableReplacements/LabelSwitch.xaml.cs
@@ -0,0 +1,27 @@
+namespace Spindler.Views.TableReplacements;
+
+public partial class LabelSwitch : ContentView
+{
+    public static readonly BindableProperty TextProperty =
+                BindableProperty.Create(nameof(Text), typeof(string), typeof(LabelSwitch));
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public static readonly BindableProperty OnProperty =
+                BindableProperty.Create(nameof(On), typeof(bool), typeof(LabelSwitch));
+
+    public bool On
+    {
+        get => (bool)GetValue(OnProperty);
+        set => SetValue(OnProperty, value);
+    }
+
+    public LabelSwitch()
+	{
+		InitializeComponent();
+	}
+}
