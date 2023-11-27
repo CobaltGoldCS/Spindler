5b268c56a53921e3e1b39841746fbeac2f1ace1b
Dylan Green <cobaltgoldcsprinks@gmail.com>
2023-11-27 11:24:43 -0700

    Refactoro WebUtilities

--- Spindler/Utilities/WebUtilities.cs
+++ Spindler/Utilities/WebUtilities.cs
@@ -1,71 +1,64 @@
-﻿namespace Spindler.Utilities
-{
-    public class WebUtilities
-    {
-        private Uri? baseUrl;
-        private Uri BaseUrl 
-        {
-            get
-            {
-                if (baseUrl is null)
-                { 
-                    throw new ArgumentNullException(nameof(BaseUrl));
-                }
-                return baseUrl;
-            }
-            set
-            {
-                if (baseUrl is null)
-                {
-                    baseUrl = value;
-                }
-            }
-        }
-        public WebUtilities(Uri baseUrl)
-        {
-            BaseUrl = baseUrl;
-        }
+﻿namespace Spindler.Utilities;
 
-        public WebUtilities()
+public class WebUtilities
+{
+    private Uri? baseUrl;
+    private Uri BaseUrl 
+    {
+        get
         {
+            ArgumentNullException.ThrowIfNull(baseUrl);
+            return baseUrl;
         }
+        set
+        {
+            baseUrl ??= value;
+        }
+    }
+    public WebUtilities(Uri baseUrl)
+    {
+        BaseUrl = baseUrl;
+    }
+
+    public WebUtilities()
+    {
+    }
 
 #pragma warning  disable CS8601
-        /// <summary>
-        /// Check if <paramref name="url"/> is in a valid http or https format
-        /// </summary>
-        /// <param name="url">The url to test validity of</param>
-        /// <returns>if <paramref name="url"/> is valid</returns>
-        public static bool IsUrl(string? url)
-        {
-            if (url == "#")
-                return false;
-            bool created = Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out Uri _);
-            return created && (url!.StartsWith("http") || url.StartsWith('/'));
-        }
+    /// <summary>
+    /// Check if <paramref name="url"/> is in a valid http or https format
+    /// </summary>
+    /// <param name="url">The url to test validity of</param>
+    /// <returns>if <paramref name="url"/> is valid</returns>
+    public static bool IsUrl(string? url)
+    {
+        if (url == "#")
+            return false;
+        bool created = Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out Uri _);
+        return created && (url!.StartsWith("http") || url.StartsWith('/'));
+    }
 #pragma warning restore CS8601
 
-        public Uri MakeAbsoluteUrl(Uri url)
-        {
-            if (url.IsAbsoluteUri) return url;
-            return new Uri(BaseUrl, url!);
-        }
-
-        public Uri MakeAbsoluteUrl(string url)
-        {
-            Uri modifierUrl = new Uri(url, UriKind.RelativeOrAbsolute);
-            return MakeAbsoluteUrl(modifierUrl);
-        }
-
-        public void SetBaseUrl(Uri baseUrl)
-        {
-            if (HasBaseUrl())
-            {
-                return;
-            }
-            BaseUrl = baseUrl;
-        }
-
-        public bool HasBaseUrl() => IsUrl(baseUrl?.ToString());
+    public Uri MakeAbsoluteUrl(Uri url)
+    {
+        if (url.IsAbsoluteUri) return url;
+        return new Uri(BaseUrl, url!);
     }
+
+    public Uri MakeAbsoluteUrl(string url)
+    {
+        Uri modifierUrl = new Uri(url, UriKind.RelativeOrAbsolute);
+        return MakeAbsoluteUrl(modifierUrl);
+    }
+
+    public void SetBaseUrl(Uri baseUrl)
+    {
+        if (HasBaseUrl())
+        {
+            return;
+        }
+        BaseUrl = baseUrl;
+    }
+
+    public bool HasBaseUrl() => IsUrl(baseUrl?.ToString());
 }
