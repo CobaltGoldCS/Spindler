9d3552543a9b297ed4c1d5b793b8412da50fb058
Dylan Green <cobaltgoldcsprinks@gmail.com>
2024-02-24 13:23:32 -0700

    Use Warning Color for Invalid Text in TextValidationBehavior

--- Spindler/Behaviors/TextValidationBehavior.cs
+++ Spindler/Behaviors/TextValidationBehavior.cs
@@ -4,11 +4,13 @@ public class TextValidationBehavior : Behavior<Entry>
 {
     public Func<string, bool> validationFunction;
     private Color textColor = Colors.Black;
+    private Color invalidTextColor;
     public TextValidationBehavior(Func<string, bool> validationFunction)
     {
         this.validationFunction = validationFunction;
         ResourceDictionary mergedDictionaries = Application.Current!.Resources;
         textColor = (Color)mergedDictionaries["TextColor"];
+        invalidTextColor = (Color)mergedDictionaries["Warning"];
     }
     protected override void OnAttachedTo(Entry entry)
     {
@@ -26,6 +28,6 @@ public class TextValidationBehavior : Behavior<Entry>
     void OnEntryTextChanged(object? sender, TextChangedEventArgs args)
     {
         bool isValid = validationFunction.Invoke(args.NewTextValue);
-        ((Entry)sender!).TextColor = isValid ? textColor : Colors.Red;
+        ((Entry)sender!).TextColor = isValid ? textColor : invalidTextColor;
     }
 }
